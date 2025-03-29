fffc457cf704933c5aea9323210d888ccaaa2a5b
Krunoslav Husak <kruno@husak.me>
2025-03-29 09:00:47 +0100

    Enhance auth middleware logging to include client IP address

--- app/middlewares/auth_middleware.go
+++ app/middlewares/auth_middleware.go
@@ -17,20 +17,20 @@ type AuthMiddleware struct {
 func (am *AuthMiddleware) New(c *raptor.Context, next func(*raptor.Context) error) error {
 	authHeader := c.Request().Header.Get("Authorization")
 	if authHeader == "" {
-		am.Log.Debug("Missing auth header")
+		am.Log.Debug("Missing auth header", "ip", c.RealIP())
 		err := errs.NewErrorUnauthorized("Missing auth header")
 		return c.JSONError(err)
 	}
 
 	parts := strings.Split(authHeader, " ")
 	if len(parts) != 2 || parts[0] != "Bearer" {
-		am.Log.Debug("Invalid auth header")
+		am.Log.Debug("Invalid auth header", "ip", c.RealIP())
 		err := errs.NewErrorUnauthorized("Invalid auth header")
 		c.JSONError(err)
 	}
 
 	if authKey := parts[1]; authKey != am.Auth.Token {
-		am.Log.Debug("Invalid auth token")
+		am.Log.Debug("Invalid auth token", "ip", c.RealIP())
 		err := errs.NewErrorUnauthorized("Invalid auth token")
 		return c.JSONError(err)
 	}
